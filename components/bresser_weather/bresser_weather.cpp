14b1ef85c574e6d6e0f1f3bb1851c9faac0e5f1b
tobox <tobox@gmx.de>
2026-04-27 21:23:47 +0200

    Fix log message for ignored sensor ID filtering

--- components/bresser_weather/bresser_weather.cpp
+++ components/bresser_weather/bresser_weather.cpp
@@ -31,7 +31,7 @@ namespace esphome
                 // Check if filter is enabled and if sensor ID matches
                 if (this->filter_enabled_ && this->ws_.sensor[i].sensor_id != this->filter_sensor_id_)
                 {
-                    ESP_LOGD(TAG, "Ignoring XX sensor ID %d (filter: %d)",
+                    ESP_LOGD(TAG, "Ignoring Xx sensor ID %d (filter: %d)",
                              this->ws_.sensor[i].sensor_id,
                              this->filter_sensor_id_);
                     return;
